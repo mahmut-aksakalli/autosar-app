b2ffe0cdedfa4bd5f45aa64aae06e3d696740306
mahmut-aksakalli <mahmutaksakalli01@gmail.com>
2026-09-18 17:23:04 +0200

    add italic text and close button for preview tab on editor

--- webview/src/components/EditorTabs/EditorTabsView.tsx
+++ webview/src/components/EditorTabs/EditorTabsView.tsx
@@ -12,18 +12,26 @@ interface EditorTabsProps {
 export function EditorTabs(props: EditorTabsProps) {
   return (
     <div className="editor-tabs">
-      {props.tabs.map((tab) => (
-        <div key={tab.id} className={`editor-tab ${tab.id === props.activeTabId ? "active" : ""}`}>
-          <button
-            type="button"
-            className="editor-tab-button"
-            onClick={() => props.onActivate(tab.id)}
-            onDoubleClick={() => props.onPin(tab)}
-            title={tab.pinned ? tab.title : `${tab.title} (preview)`}
-          >
-            {tab.title}
-          </button>
-          {props.tabs.length > 1 && (
+      {props.tabs.map((tab) => {
+        let tabClassName = "editor-tab";
+        if (tab.id === props.activeTabId) {
+          tabClassName += " active";
+        }
+        if (!tab.pinned) {
+          tabClassName += " preview";
+        }
+
+        return (
+          <div key={tab.id} className={tabClassName}>
+            <button
+              type="button"
+              className="editor-tab-button"
+              onClick={() => props.onActivate(tab.id)}
+              onDoubleClick={() => props.onPin(tab)}
+              title={tab.pinned ? tab.title : `${tab.title} (preview)`}
+            >
+              {tab.title}
+            </button>
             <button
               type="button"
               className="editor-tab-close"
@@ -32,9 +40,9 @@ export function EditorTabs(props: EditorTabsProps) {
             >
               x
             </button>
-          )}
-        </div>
-      ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
